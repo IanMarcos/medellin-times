5ae96009433162b4e85d0060da69946793c50bdd
Ian Ortiz <ian.marcos.ortiz@gmail.com>
2024-05-10 18:21:27 -0500

    feat: created form controls for the news form

--- frontend/src/app/components/news-form/news-form.component.ts
+++ frontend/src/app/components/news-form/news-form.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-news-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './news-form.component.html',
-  styleUrl: './news-form.component.css'
+  styleUrl: './news-form.component.css',
 })
 export class NewsFormComponent {
+  newsForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    subtitle: new FormControl(''),
+    content: new FormControl('', Validators.required),
+    author: new FormControl('', Validators.required),
+  });
 
+  handleSubmit() {
+    if (this.newsForm.status === 'INVALID') {
+      return;
+    }
+  }
 }
